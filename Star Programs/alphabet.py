289be733c173b299b961100608bb693f8c452af7
Monk <mayankmehra667@gmail.com>
2021-09-29 00:24:26 +0530

    add more

---
+++ Star Programs/alphabet.py
@@ -0,0 +1,8 @@
+n = int(input())
+num = 65
+for i in range(0,n):
+    num = 65
+    for j in range(0,i+1):
+        print(num, end=" ")
+        num = num+1
+    print("\r")
---
+++ Star Programs/print_a_to_z.py
@@ -0,0 +1,7 @@
+import string
+print("Alphabet from a-z:")
+for letter in string.ascii_lowercase:
+       print(letter,"\n", end =" ")
+       
+for letter in string.ascii_uppercase:
+   print(letter,"\n", end = " ")
